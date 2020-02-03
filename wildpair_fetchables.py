4a23a57b8099b2ab0cec3305ee513e7f9b92d813
ElementUser <w.lam@honteam.com>
2020-02-02 22:07:41 -0500

    Populated creature decklist manually, added basic  Deckstats API call (unused for now). Now prints out the final desired string =)

--- wildpair_fetchables.py
+++ wildpair_fetchables.py
@@ -6,15 +6,34 @@
 
 # Wild Pair card details: https://gatherer.wizards.com/pages/card/Details.aspx?multiverseid=416953
 
-import scrython
+import scrython, requests
 
 # Decklist from my Arcades deck: https://deckstats.net/decks/144326/1493742-arcades-wall-midrange-aggro/en
 
+# Deckstats API calls
+# URL = "https://deckstats.net/api.php"
+
+# PARAMS = {
+#     "action":  "get_deck",
+#     "id_type": "saved",
+#     "owner_id": 144326,
+#     "id": 1493742,
+#     "response_type": "json",
+# }
+
+# response = requests.get(url=URL, params=PARAMS)
+# print(response.text)
+
+
+# Temp stuff
 deckCreatureList = [
-    "Angelic Wall", "Axebane Guardian"
+    "Angelic Wall", "Arcades, the Strategist", "Axebane Guardian", "Birds of Paradise", "Carven Caryatid", "Consulate Skygate", "Crashing Drawbridge", "Drift of Phantasms", "Fortified Rampart", "Glacial Wall", "Hover Barrier", "Jeskai Barricade", "Jungle Barrier", 
+    "Marble Titan", "Oathsworn Giant", "Orator of Ojutai", "Overgrown Battlement", "Perimeter Captain", "Resolute Watchdog", "Shield Sphere", "Stalwart Shield-Bearers", "Sunscape Familiar", "Tetsuko Umezawa, Fugitive", "Tree of Redemption",
+    "Vine Trellis", "Wall of Blossoms", "Wall of Denial", "Wall of Frost", "Wall of Junk", "Wall of Mulch", "Wall of Omens", "Wall of Runes", "Wall of Shards", "Wall of Stolen Identity", "Wall of Tanglecord", "Wall of Tears", "Wall of Vines"
 ]
 
 totalPowerToughnessDict = {}
+finalString = "Power & Toughness Sum map for Wild Pair:\n"
 
 for creature in deckCreatureList:
     card = scrython.cards.Named(fuzzy=creature)
@@ -24,7 +43,11 @@ for creature in deckCreatureList:
     else:
         totalPowerToughnessDict[str(sumPowerToughness)].append(card.name())
 
-for powerToughnessScore in totalPowerToughnessDict:
+# sorted(dict) sorts the dictionary by key (ascending)
+for powerToughnessScore in sorted(totalPowerToughnessDict):
+    finalString += "\n" + powerToughnessScore + ": "
     for creature in totalPowerToughnessDict[powerToughnessScore]:
-        print(powerToughnessScore)
-        print(creature)
+        finalString += creature + ", "
+    finalString = finalString.rstrip(', ')
+
+print(finalString)
